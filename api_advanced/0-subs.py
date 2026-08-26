2487370cd8a18a262e6cbfad73fc64d2e100daa4
Mizeroo <l.mizero@alustudent.com>
2026-08-26 13:38:30 +0400

    Fix indentation in 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "linux:api.advanced.project:v1.0"}
+    headers = {"User-Agent": "linux:api.advanced.project:v1.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
